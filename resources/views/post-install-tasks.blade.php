2c8e7d100c20fe8ad5b1a76175e74cf7b8306605
Christoph Rene Pardon <rene.pardon@boonweb.de>
2023-01-10 11:13:58 +0100

    chore (CLP) BW-0: adjust post install task information

--- resources/views/post-install-tasks.blade.php
+++ resources/views/post-install-tasks.blade.php
@@ -13,7 +13,7 @@
     <ul>
         <li>Run `cd {{$appName}}`</li>
         <li>(option 1) Adjust your .env file to contain proper database settings and run `php artisan migrate`</li>
-        <li>(option 2) Run `php artisan sail:install && sail up -d && sail artisan migrate`</li>
+        <li>(option 2) Run `php artisan sail:install && sail up -d && sail artisan db:wipe && sail artisan migrate`</li>
         <li>Run `(sail) {{$packageManager}} install`</li>
         <li>Run `(sail) {{$packageManager}} run dev`</li>
     </ul>
